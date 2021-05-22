c332f99008f3ad7058be01990ed2f8359fb2d532
Tinaël Devresse <32441291+HunteRoi@users.noreply.github.com>
2021-05-22 10:18:06 +0200

    Remove OnInit

--- src/app/invoices/containers/invoices/invoices.component.ts
+++ src/app/invoices/containers/invoices/invoices.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, OnInit } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { Invoice } from '../../models/invoice';
@@ -9,14 +9,10 @@ import { InvoicesService } from '../../services/invoices.service';
   templateUrl: './invoices.component.html',
   styleUrls: ['./invoices.component.css']
 })
-export class InvoicesComponent implements OnInit, OnChanges {
+export class InvoicesComponent implements OnChanges {
   invoices$: Observable<Invoice[]>;
 
   constructor(private invoicesService: InvoicesService) {}
-
-  ngOnInit(): void {
-    this.invoices$ = this.invoicesService.getInvoices$();
-  }
 
   ngOnChanges() {
     this.invoices$ = this.invoicesService.getInvoices$();
